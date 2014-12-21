03fa1b043d5737110efa78530db9667b9d3b4aba
Armando Diaz Tolentino <ajdt@pikachu>
2014-12-21 12:30:59 -0800

    Simplified args definitionin grammar. Created Predicate class and DefinedPredicate classes

--- antlr/PrologRules.g4
+++ antlr/PrologRules.g4
@@ -3,8 +3,27 @@ grammar PrologRules;
 prologrule  :           predicate   ':-' predlist  '.';
 predicate   :           atom '(' args ')' | atom ;
 predlist    :           predicate ',' predlist | predicate ;
-args        :           atom | atom ',' args ;
+args        :           atom (',' atom)* ;
 atom        :           WORD ;
 
 WORD        :   [_a-zA-Z][_a-zA-Z0-9]* ;
+
+/*
+grammar PrologRules;
+
+prologrule  returns [rule_data]
+    :           predicate   ':-' predlist  '.'
+                { $rule_data = ($predicate.text, $predlist.list_of_preds) };
+predicate   returns [text]
+    :           atom '(' args ')' {$text = ($atom.name, $args.vallist)} | atom {$text = ($atom.name, None) } ;
+predlist    returns [list_of_preds]
+    :           predicate ',' predlist {$list_of_preds = [$predicate.text] + $predlist.list_of_preds}
+                | predicate {$list_of_preds = [$predicate.text] };
+args        returns [vallist]       
+    :           atom {$vallist = [$atom.name]} | atom ',' args {$vallist = [$atom.name] + $args.vallist};
+atom        returns [name]
+    :           WORD {$name = $WORD.text } ;
+
+WORD        :   [_a-zA-Z][_a-zA-Z0-9]* ;
+*/
 
--- antlr/parse_asp_rules.py
+++ antlr/parse_asp_rules.py
@@ -2,6 +2,17 @@ import sys
 from antlr4 import *
 from PrologRulesLexer import *
 from PrologRulesParser import *
+
+from collections import namedtuple
+Predicate   =   namedtuple('Predicate', ['name', 'args', 'arity'])
+class DefinedPredicate(Predicate):
+    """A DefinedPredicate is just an ASP rule"""
+    def __init__(self, name, args, arity, defining_predicates):
+        Predicate.__init__(name, args, arity)
+        self.def_predicates = defining_predicates
+    def __str__(self):
+        return Predicate.__str__(self) + 'definiton: ' + ','.join([p.__str__ for p in self.def_predicates])
+
 #prolog_file = FileStream('rules.lp')
 prolog_file = FileStream(sys.argv[1])
 lexer = PrologRulesLexer(prolog_file)
@@ -15,27 +26,28 @@ class KeyPrinter(PrologRulesListener):
     def __init__(self):
         super(KeyPrinter, self).__init__()
         self.tree = []
-        self.current_pred = None
-        self.current_args = None
-        self.current_container = None
+        self.container_queue = [self.tree]
     def enterPredicate(self, ctx):
-        self.current_pred = []
-        self.current_container = self.current_pred
+        self.container_queue.append([])
     def exitPredicate(self, ctx):
-        self.tree.append(self.current_pred)
-        self.current_pred = None
-        self.current_container = None
+        parsed_pred = self.container_queue.pop()
+        name = parsed_pred[0]
+        if len(parsed_pred) > 1:
+            args = parsed_pred[1]
+            arity = len(args)
+        else:
+            args = None
+            arity = 0 
+        pred_obj = Predicate(name, args, arity)
+        self.container_queue[-1].append(pred_obj)
     def enterArgs(self, ctx):
-        self.current_args = []
-        self.current_container = self.current_args
+        self.container_queue.append([])
     def exitArgs(self, ctx):
-        if self.current_args != None:
-            self.current_pred.append(self.current_args)
-        self.current_args = None
-        self.current_container = self.current_pred
+        arg_list = self.container_queue.pop()
+        if len(arg_list) > 0:
+            self.container_queue[-1].append(arg_list)
     def exitAtom(self, ctx):
-        if self.current_container != None:
-            self.current_container.append(str(ctx.WORD()))
+        self.container_queue[-1].append(str(ctx.WORD()))
 
 # print some info rpom parsing
 printer = KeyPrinter()
@@ -53,3 +65,4 @@ print printer.tree
     # has a generate explanations field
     # (will be added to a dict that indexes based on name and arity)
 # args: can just be a tuple or list, remember to preserve order
+        
