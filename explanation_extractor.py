f7aa339baf89ec17ed5e48e9a04012c651771820
Armando Diaz Tolentino <ajdt@pikachu>
2015-01-09 14:58:06 -0800

    Explanations are now parsed from predicate name. Need to be integrated into explanation template

--- explanation_extractor.py
+++ explanation_extractor.py
@@ -22,7 +22,8 @@ def makeSanitizedFile(file_name):
     file_obj    = open(file_name, 'r')
     code        = re.sub('%.*\n', '\n', file_obj.read()) # remove comments
 
-    #new_file_obj   = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    new_file_obj   = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    # extract each matching corresponding to a rule
     for matching in re.findall(r'[\w]+\(?[^\.:]*:-[^\.]*\.', code):
         if len(matching.split(':-')) > 2:
             continue
@@ -32,7 +33,39 @@ def makeSanitizedFile(file_name):
     new_file_obj.close()
     return new_file_obj.name
 
+def parseRules():
+    temp_file_name  = makeSanitizedFile('rules.lp')
+    rules_list      = par.parseRulesFromFile(temp_file_name)
 
+    for rule in rules_list:
+        print makeExplanationForRule(rule)
+
+    os.remove(temp_file_name)
+
+def makeExplanationForRule(rule):
+    return [ makeConditionExplanation(cond) for cond in rule.body]
+
+# each of these returns a list with text explanations
+def makeConditionExplanation(condition, stop_depth=0, depth=0):
+    if isinstance(condition, par.Predicate):
+        return makePredicateExplanation(condition, stop_depth, depth)
+def makePredicateExplanation(predicate, stop_depth=0, depth=0):
+    # for now just generate explanation for this predicate ignore depth input
+    return convertFromCamelCase(predicate.name)
+def makeComparisonExplanation(comparison, stop_depth=0, depth=0):
+    pass
+def makePredcountExplanation(predcount, stop_depth=0, depth=0):
+    pass
+
+
+## code to convert camelcase to proper explanation
+def convertFromCamelCase(name):
+    """ Takes a string like 'ILikePuppies' and returns 'i like puppies' """
+    # NOTE: this code is not mine!!
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    s2 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    s3 = re.sub('_', ' ', s2)
+    return s3
 
 
 # extract predicate and variables from a given
@@ -45,4 +78,29 @@ def main():
     print 'file now deleted'
 
 if __name__ == '__main__':
-    main()
+    #main()
+    parseRules()
+
+
+# code design for generating explanations
+# extractExplanationForRule -- given rule create an explanation template for it: shold also have a depth parameter to suggest how much detail to go into
+# extractExplanationForPredicate -- see above, but works only for predicates recursively calls extractExplanationForRule if necessary
+# extractExplanationForPredCount
+# extractExplanationForComparison
+# template class? contains list of all variables, can substitute values in for the variables?
+#   stores predicate that it defines, stores explanation code, stores depth level of the explanation
+#   how do the variables get matched to equation elements? Decide this later
+#   stores elements as a list
+#   whatever calls explanation generation code creates this template 
+# code to process predicate explanations:
+#   convert camel case to underscores, split and then join with spaces
+#   include place for variables
+# need global container for all rules that have been parse!!
+
+
+# Code TODOs
+    # XX be able to generate explanations first
+    # fix mismatched input errors
+    # create template class for each parsed rule
+    # merge multiple explanations for same predicate
+    # create a manager class to deal with lookups of predicates etc
